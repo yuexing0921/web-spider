f8cc394b013bec77ef52600b96be4488906e169f
yuexing0921 <yuexing0921@gmail.com>
2016-06-05 20:59:56 +0800

    add logger

---
+++ src/common/logger.js
@@ -0,0 +1,102 @@
+/**
+ * Created by yuexing on 2016/6/5.
+ * 这是仿照log4js做的一个简单的日志系统，方便调试
+ */
+let Levels = {
+	TRACE : 0,
+	DEBUG : 1,
+	INFO  : 2,
+	WARN  : 3,
+	ERROR : 4,
+	FATAL : 5
+};
+let level = Levels.INFO;
+class logger {
+	static _log(type,_arguments){
+		var args = Array.prototype.slice.call(_arguments);
+		if (args.length > 1) {
+			var i = 1, hasstyle = false;
+			if (args[0].indexOf("%c") == 0) {
+				args[0] = args[0].replace(/%c/, "");
+				i = 2;
+				hasstyle = true;
+			}
+			for (; i < args.length; i++) {
+				if (/%s|%d|%i|%o|%j/.test(args[0])) {
+					args[0] = args[0].replace(/%s|%d|%i|%o|%j/, args[i]);
+				}
+				else {
+					break;
+				}
+			}
+			if (i < args.length) {
+				args[0] = args[0] + " " + args.slice(i).join(" ");
+			}
+			if (hasstyle) {
+				console[type](args[0], args[1]);
+			}
+			else {
+				console[type](args[0]);
+			}
+		}
+		else if (args.length == 1) {
+			if (_arguments[0] instanceof Array) {
+				console[type]("[" + args[0] + "]");
+			}
+			else if (_arguments[0] instanceof Function) {
+				console[type](args[0], null, "console_log_function");
+			}
+			else {
+				console[type](args[0]);
+			}
+		}
+		else {
+			console[type]("");
+		}
+	}
+	static trace() {
+		if(Levels.TRACE >= level){
+			this._log("log",arguments);
+		}
+	}
+
+	static debug() {
+		if(Levels.DEBUG >= level){
+			this._log("log",arguments);
+		}
+	}
+
+	static info() {
+		if(Levels.INFO >= level){
+			this._log("info",arguments);
+		}
+	}
+
+	static warn() {
+		if(Levels.WARN >= level){
+			this._log("warn",arguments);
+		}
+	}
+
+	static error() {
+		if(Levels.ERROR >= level){
+			this._log("error",arguments);
+		}
+	}
+
+	static fatal() {
+		this._log("error",arguments);
+	}
+
+	static setLevel(type){
+		if(!type || typeof type !== 'string'){
+			return ;
+		}
+		let l = Levels[type];
+		if(level){
+			level = l;
+		}
+	}
+}
+logger.Levels = Levels;
+exports = module.exports = logger;
